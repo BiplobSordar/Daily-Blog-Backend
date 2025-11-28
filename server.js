b83dfdd40da1abdeed7a1fa1e953030142d618ad
unknown <biplob.code.bd@gmail.com>
2025-11-28 16:34:03 +0600

    fix cros

--- server.js
+++ server.js
@@ -11,7 +11,22 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = ["https://daily-blog-liard.vercel.app"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+     
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, 
+  })
+);
 app.use(express.json());
 app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message || "Server Error" });
